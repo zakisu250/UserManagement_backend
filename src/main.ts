7ddd0903740cfb3cb2f7602551483a240151cc71
zakisu250 <zakisu250@gmail.com>
2023-10-12 19:54:29 +0300

    configured app port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(port);
 }
 bootstrap();
